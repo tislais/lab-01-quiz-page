6d8d038e1f49fb8249ac341a5c89e3b09010c0a1
tislais <tistististis@gmail.com>
2021-03-23 17:35:33 -0700

    add images and start of app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
+import {
+    isYes
+} from './utils.js';
 
 // initialize state
 
+const startQuizButton = document.getElementById('start-quiz-button');
+const quizResultsButton = document.getElementById('quiz-results-button');
+
 // set event listeners to update state and DOM
+
+startQuizButton.addEventListener('click', () => {
+    alert('Welcome to the Twisty Puzzle Quiz!!');
+})
