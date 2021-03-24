e928b4abda510bab78297f948132593d1f07dbc9
tislais <tistististis@gmail.com>
2021-03-23 17:34:35 -0700

    add tests and utils.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,23 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import {
+    isYes
+} from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('a function that returns true when the user inputs an answer that starts with Y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = isYes('yeah');
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual, expected, 'yes returns true');
+    expect.equal(isYes('YEEEEE'), true, 'YEEEEE returns true');
+    expect.equal(isYes('nyEaaupP'), false, 'nyEaaupP returns false');
 });
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function isYes(userInput) {
+    return userInput[0].toLowerCase() === 'y';
+}
